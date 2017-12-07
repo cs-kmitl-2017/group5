f209ad7e00cac7b12e514fd6d976cda943c8bb15
nontakho <nontakho@gmail.com>
2017-12-07 23:47:33 +0700

    I create model of head docment tax invoice.It provide attribute on head document

--- src/main/java/th/ac/kmitl/science/comsci/example/models/HeadForms.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/HeadForms.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package th.ac.kmitl.science.comsci.example.models;
 
-/**
- *
- * @author MicroComSci
- */
 public class HeadForms {
     
     private String ExchangedDocument;
