7f4f1449f9ef4034ec097b56a1492c07fdb4989c
Chayapol Limanon <chayapol.limanon@gmail.com>
2017-12-08 20:33:11 +0700

    Create UniversalCommunication, EmailAddress, and Trader models with mocks and tests.

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Trader.java
@@ -0,0 +1,93 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trader {
+
+    private String id;
+    private String globalId;
+    private String name;
+    private String taxId;
+    private List<UniversalCommunication> definedTradeContacts;
+    private PostalAddress postalTradeAddress;
+
+    public Trader(
+            String id,
+            String globalId,
+            String name,
+            String taxId) {
+        this();
+        this.setId(id);
+        this.globalId = globalId;
+        this.name = name;
+        this.taxId = taxId;
+    }
+
+    public Trader() {
+        setDefinedTradeContacts(new ArrayList<UniversalCommunication>());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    protected void setId(String id) {
+        if(id == null || id.isEmpty())
+            throw new UnsupportedOperationException("This field cannot be null or empty");
+
+        this.id = id;
+    }
+
+    public String getGlobalId() {
+        return globalId;
+    }
+
+    protected void setGlobalId(String globalId) {
+        this.globalId = globalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    protected void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public List<UniversalCommunication> getDefinedTradeContacts() {
+        return definedTradeContacts;
+    }
+
+    private void setDefinedTradeContacts(List<UniversalCommunication> definedTradeContacts) {
+        this.definedTradeContacts = definedTradeContacts;
+    }
+
+    public PostalAddress getPostalTradeAddress() {
+        return postalTradeAddress;
+    }
+
+    public void setPostalTradeAddress(PostalAddress postalTradeAddress) {
+        this.postalTradeAddress = postalTradeAddress;
+    }
+
+    public Trader withDefinedTradeContacts(UniversalCommunication... traderContacts) {
+        for (UniversalCommunication traderContact : traderContacts) {
+            getDefinedTradeContacts().add(traderContact);
+        }
+        return this;
+    }
+
+    public Trader withPostalTradeAddress(PostalAddress postalTradeAddress) {
+        setPostalTradeAddress(postalTradeAddress);
+        return this;
+    }
+}
---
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/TraderTest.java
@@ -0,0 +1,91 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import th.ac.kmitl.science.comsci.example.mock.PostalAddressMock;
+import th.ac.kmitl.science.comsci.example.mock.UniversalCommunicationMock;
+import th.ac.kmitl.science.comsci.example.mock.TraderMock;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+import static th.ac.kmitl.science.comsci.example.mock.PostalAddressMock.postalAddressWithSomeAttribute;
+import static th.ac.kmitl.science.comsci.example.mock.TraderMock.*;
+
+public class TraderTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void canCreateTrader() {
+        Trader trader = TraderMock.trader();
+
+        assertEquals(id, trader.getId());
+        assertEquals(globalId, trader.getGlobalId());
+        assertEquals(name, trader.getName());
+        assertEquals(taxId, trader.getTaxId());
+    }
+
+    @Test
+    public void canCreateTraderWithContacts() {
+        List<UniversalCommunication> universalCommunications = UniversalCommunicationMock.generateMockUniversalCommunicationList();
+
+        Trader trader = TraderMock.trader()
+                .withDefinedTradeContacts(
+                        universalCommunications.get(0),
+                        universalCommunications.get(1)
+                );
+
+        assertEquals(id, trader.getId());
+        assertEquals(globalId, trader.getGlobalId());
+        assertEquals(name, trader.getName());
+        assertEquals(taxId, trader.getTaxId());
+        assertEquals(true, trader.getDefinedTradeContacts().containsAll(universalCommunications));
+    }
+
+    @Test
+    public void canCreateTraderWithPostalAddress() {
+
+        PostalAddress postalAddress = postalAddressWithSomeAttribute();
+
+        Trader trader = TraderMock.trader()
+                .withPostalTradeAddress(postalAddress);
+
+        assertEquals(id, trader.getId());
+        assertEquals(globalId, trader.getGlobalId());
+        assertEquals(name, trader.getName());
+        assertEquals(taxId, trader.getTaxId());
+        assertEquals(postalAddress, trader.getPostalTradeAddress());
+    }
+
+    @Test
+    public void cannotCreateWithNullId() {
+        exception.expect(UnsupportedOperationException.class);
+        Trader trader = new Trader(null, globalId, name, taxId);
+    }
+
+    @Test
+    public void cannotCreateWithEmptyId() {
+        exception.expect(UnsupportedOperationException.class);
+        Trader trader = new Trader("", globalId, name, taxId);
+    }
+
+    @Test
+    public void cannotSetIdWithNull() {
+        Trader trader = trader();
+
+        exception.expect(UnsupportedOperationException.class);
+        trader.setId(null);
+    }
+
+    @Test
+    public void cannotSetIdWithNullOrEmpty() {
+        Trader trader = trader();
+
+        exception.expect(UnsupportedOperationException.class);
+        trader.setId("");
+    }
+}
